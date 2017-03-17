494b0294729f814108e57ddb115b3dc1e06c19c5
Jie Wei <email2jie@gmail.com>
2017-03-17 15:34:03 -0700

    changed default profile

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {repos: [], owner: {}};
-    this.onChangeFunc('email2jie');
+    this.onChangeFunc('potato');
 
   }
 
