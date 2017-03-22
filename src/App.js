3d035d7d45b9c1e5afeb48ce63c3898b7554d413
Jie Wei <email2jie@gmail.com>
2017-03-22 01:03:35 -0700

    error messages 2.0

--- src/App.js
+++ src/App.js
@@ -31,7 +31,13 @@ class App extends Component {
       .then(value => this.setState({error: false, repos: value, owner: value[0].owner}))
       .catch(error => {
         console.dir(error);
-        this.setState({error: true});
+        if(error.message === "Forbidden"){
+          this.setState({error: "Forbidden"});
+        }else if(error.message === "Not Found"){
+          this.setState({error: "Not Found"});
+        }else{
+          this.setState({error: "true"});
+        }
       });
   }
 
@@ -48,6 +54,7 @@ class App extends Component {
     let language = null;
     let error = null;
     let owner = null;
+    let input = null;
     if (this.state.repos.length > 0){
       owner = <ul className=''>
         <li>Username: {this.state.owner.login}</li>
@@ -67,8 +74,20 @@ class App extends Component {
         <LanguageData owner={this.state.owner} repoList={this.state.repos} />
       </div>
       }
-      if(this.state.error){
-        error = <div>Error: User not found</div>
+      if(this.state.error === "Not Found"){
+          error = <div>Error: User not found</div>
+      }else if(this.state.error === "Forbidden"){
+          error = <div>Error: API Limit Reached. Try again in an hour.</div>
+          input = null;
+      }else if(this.state.error === 'true'){
+          error = <div>Error: Something went wrong.</div>
+      }else{
+        input =
+      <form className='pure-form' onSubmit={this.onHandleSubmit}>
+        <label for='name'>
+          <input id='name' type='text' placeholder="Search Github User" />
+        </label>
+      </form>
       }
 
 return (
@@ -83,12 +102,7 @@ return (
     <div className="pure-u-1-6">
     </div>
     <div className="pure-u-1-3">
-      <form className='pure-form' onSubmit={this.onHandleSubmit}>
-        <label for='name'>
-          <input id='name' type='text' placeholder="Search Github User" />
-        </label>
-      </form>
-
+      {input}
       {avatar}
       {owner}
       {language}
